b4664a689e836063e8501a111fc5983e384bef37
root <root@localhost>
2025-09-23 02:23:14 +0000

    Rails review

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,7 +296,7 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  if Rails.env.production?
+  if Rails.env.production? || Rails.env.review?
     config.omniauth :google_oauth2,
       ENV['GOOGLE_OAUTH_CLIENT_ID'],
       ENV['GOOGLE_OAUTH_CLIENT_SECRET'],
