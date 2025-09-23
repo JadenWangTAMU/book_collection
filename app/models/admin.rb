59b189e2f2fcde15590694930fe31e8d65fc252f
root <root@localhost>
2025-09-23 03:02:58 +0000

    admin logging

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,21 @@
-class Admin < ApplicationRecord
-  devise :omniauthable, omniauth_providers: [:google_oauth2]
+def self.from_google(email:, full_name:, uid:, avatar_url:)
+  Rails.logger.info "Admin.from_google called with email=#{email}, uid=#{uid}"
 
-  def self.from_google(email:, full_name:, uid:, avatar_url:)
-    create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
+  begin
+    admin = find_or_create_by!(email: email) do |a|
+      a.uid = uid
+      a.full_name = full_name
+      a.avatar_url = avatar_url
+    end
+
+    # In case the admin existed but we need to update info
+    admin.update(uid: uid, full_name: full_name, avatar_url: avatar_url) if admin.changed?
+
+    Rails.logger.info "Admin found or created: #{admin.inspect}"
+    admin
+  rescue => e
+    Rails.logger.error "Error in Admin.from_google: #{e.message}"
+    Rails.logger.error e.backtrace.join("\n")
+    nil
   end
-
 end
